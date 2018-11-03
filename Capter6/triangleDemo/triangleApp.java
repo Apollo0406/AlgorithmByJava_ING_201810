f2fd8bec91b7a86a9e584db3ec1f628b8a070517
Apollo0406 <2433823963@qq.com>
2018-11-03 15:19:02 +0800

    triangle demo

---
+++ Capter6/triangleDemo/triangleApp.java
@@ -0,0 +1,35 @@
+package triangleDemo;
+//三角数字
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class triangleApp {
+    static int theNumber;
+
+    public static void main(String[] args) throws IOException{
+        System.out.print("Enter a number:");
+        theNumber = getInt();
+        int theAnswer = triangle(theNumber);
+        System.out.println("triangle="+theAnswer);
+    }
+
+    public static int triangle(int n){
+        if(n == 1)
+            return 1;
+        else
+            return (n + triangle(n-1));
+    }
+
+    public static String getString() throws IOException{
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        String s = br.readLine();
+        return s;
+    }
+
+    public static  int getInt() throws IOException{
+        String s = getString();
+        return Integer.parseInt(s);
+    }
+}
